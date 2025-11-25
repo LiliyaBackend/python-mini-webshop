f504e117d1a8ae9a199d0dda2eb432ae26a0300d
LiliyaBackend <65602011+LiliyaBackend@users.noreply.github.com>
2025-11-25 19:06:14 +0100

    Update products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,1 +1,25 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
+from ..database import SessionLocal
+from .. import crud
 
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/products", response_class=HTMLResponse)
+def products_page(request: Request, db: Session = Depends(get_db)):
+    products = crud.get_products(db)
+    return request.app.templates.TemplateResponse(
+        "products.html",
+        {
+            "request": request,
+            "products": products
+        }
+    )
