882b2461f94c5cb60d86ec14d837c0bd8aa3c499
LiliyaBackend <65602011+LiliyaBackend@users.noreply.github.com>
2025-11-25 19:01:09 +0100

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel
 
+class ProductBase(BaseModel):
+    name: str
+    price: float
+    image: str
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class OrderCreate(BaseModel):
+    items: str
+    total: float
