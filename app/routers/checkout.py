5c8479c0c9ab83920bf297f3a14af537fe976a81
LiliyaBackend <65602011+LiliyaBackend@users.noreply.github.com>
2025-11-25 19:06:53 +0100

    Update checkout.py

--- app/routers/checkout.py
+++ app/routers/checkout.py
@@ -1,1 +1,11 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 
+router = APIRouter()
+
+@router.get("/checkout", response_class=HTMLResponse)
+def checkout_page(request: Request):
+    return request.app.templates.TemplateResponse(
+        "checkout.html",
+        {"request": request}
+    )
