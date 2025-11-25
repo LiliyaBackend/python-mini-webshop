fe5351684b7fc34bea7c0f362d2cd1d5cc992cc0
LiliyaBackend <65602011+LiliyaBackend@users.noreply.github.com>
2025-11-25 19:07:30 +0100

    Update users.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
+router = APIRouter()
+
+@router.get("/user")
+def user_home():
+    return RedirectResponse("/login")
