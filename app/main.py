edab228f83f0025e705c0ec41000c5f8e168d22e
LiliyaBackend <65602011+LiliyaBackend@users.noreply.github.com>
2025-11-26 18:33:18 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 from .database import Base, engine, SessionLocal
 from . import crud
 from .routers import products, checkout, users
@@ -8,13 +9,17 @@ from . import auth, cart, orders
 
 app = FastAPI()
 
-# Статика
+# Подключаем статические файлы
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Шаблоны
-app.templates = Jinja2Templates(directory="app/templates")
+# Подключаем шаблоны
+templates = Jinja2Templates(directory="app/templates")
 
-# Роутеры
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Подключаем роутеры
 app.include_router(products.router)
 app.include_router(checkout.router)
 app.include_router(users.router)
@@ -22,20 +27,7 @@ app.include_router(auth.router)
 app.include_router(cart.router)
 app.include_router(orders.router)
 
-# Создаем таблицы и товары
+# Создаем таблицы и дефолтные товары
 Base.metadata.create_all(bind=engine)
 with SessionLocal() as db:
     crud.create_default_products(db)
-
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-
-templates = Jinja2Templates(directory="app/templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/")
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
