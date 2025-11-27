c963ceee75dac83196a337502533819f2c08b94a
LiliyaBackend <65602011+LiliyaBackend@users.noreply.github.com>
2025-11-27 18:55:42 +0100

    Update products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, Request, Depends
-from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app import crud
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="app/templates")
 
-
 @router.get("/products")
-def get_products(request: Request, db: Session = Depends(get_db)):
+def products_page(request: Request, db: Session = Depends(get_db)):
     products = crud.get_products(db)
-    return templates.TemplateResponse("products.html", {"request": request, "products": products})
+    return templates.TemplateResponse(
+        "products.html",
+        {"request": request, "products": products}
+    )
