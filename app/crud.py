cb04765e74cd5a208bc6e8e7c82f178a6443630a
LiliyaBackend <65602011+LiliyaBackend@users.noreply.github.com>
2025-11-27 19:01:06 +0100

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,15 +1,14 @@
-from sqlalchemy.orm import Session
 from . import models
-
-def get_products(db: Session):
-    return db.query(models.Product).all()
+from sqlalchemy.orm import Session
 
 def create_default_products(db: Session):
     if db.query(models.Product).count() == 0:
-        products = [
+        db.add_all([
             models.Product(name="product_1", price=1, image="product_1.jpg"),
             models.Product(name="product_2", price=2, image="product_2.jpg"),
             models.Product(name="product_3", price=3, image="product_3.jpg"),
-        ]
-        db.add_all(products)
+        ])
         db.commit()
+
+def get_all_products(db: Session):
+    return db.query(models.Product).all()
