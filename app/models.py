1af5269dcf360871112b07df8e176c30ec43a131
LiliyaBackend <65602011+LiliyaBackend@users.noreply.github.com>
2025-11-27 19:01:29 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,28 +1,10 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class Product(Base):
     __tablename__ = "products"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
-    price = Column(Float)
-    image = Column(String)
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    password = Column(String)
-
-class Order(Base):
-    __tablename__ = "orders"
-
-    id = Column(Integer, primary_key=True)
-    items = Column(String)        # просто текстовое подтверждение заказа
-    total = Column(Float)
-    user_id = Column(Integer, ForeignKey("users.id"))
-
-    user = relationship("User")
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    image = Column(String, nullable=False)
